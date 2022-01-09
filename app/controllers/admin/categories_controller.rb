630fb7eee9e875b9f2f921336da32e84281d7605
Trent Pfeiffer <trentpf@hotmail.com>
2022-01-09 14:27:02 -0500

    Add admin categories feature

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,35 @@
+class Admin::CategoriesController < ApplicationController
+
+  username = ENV["username"]
+  password = ENV["password"]
+
+  http_basic_authenticate_with name: username, password: password
+  
+  def index
+    @category = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Product.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  ## Removed destroy as not certain how it functions when there are products in the category being destroyed
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
